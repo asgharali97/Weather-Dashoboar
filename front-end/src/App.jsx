00a09519c33fd40fc3eaf20388a1946736ed439a
asgharali97 <asgharalifs48@gmail.com>
2025-07-09 15:02:33 -0700

    Done the Navbar

---
+++ front-end/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="w-full h-screen bg-[#E7E5E4] text-[#374151]">
+        <Navbar />
+      </div>
+    </>
+  )
+}
+
+export default App
