387886e7e89873d32402fc84f1fcde32d98f51f5
asgharali97 <asgharalifs48@gmail.com>
2025-09-04 20:42:45 +0500

    add the env key in api

--- front-end/src/api/weatherApi.js
+++ front-end/src/api/weatherApi.js
@@ -1,31 +1,31 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 const currentWeather = async (city) => {
-  const res = await api.post("/", { city });
+  const res = await api.post("/api/", { city });
   return res.data.data;
 };
 
 const weeklyForecast = async (city) => {
-  const res = await api.post("/weekly-forecast", { city });
+  const res = await api.post("/api/weekly-forecast", { city });
   return res.data.data;
 };
 
 const hourlyForecast = async (city) => {
-  const res = await api.post("/hourly-forecast", { city });
+  const res = await api.post("/api/hourly-forecast", { city });
   return res.data.data;
 };
 
 const historyWeather = async (city) => {
-  const res = await api.post("/history", { city });
+  const res = await api.post("/api/history", { city });
   return res.data.data;
 };
 
 const airQuality = async (city) => {
-  const res = await api.post("/air-quality", { city });
+  const res = await api.post("/api/air-quality", { city });
   return res.data.data;
 };
 
